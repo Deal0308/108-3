e84615a06a2999176b876585f9d7523057f94721
Michael Deal <Deal0308@yahoo.com>
2023-10-21 12:58:39 -0500

    new

---
+++ mock_data.py
@@ -0,0 +1,102 @@
+
+
+catalog =[
+    {
+        "title": "Blue Nikes",
+        "category": "Nike",
+        "price": 109.99,
+        "image": "images.jpeg",
+        "_id": "1" #don't repeat this
+    },
+    {
+        "title": "Grey Clouds",
+        "category": "Clouds",
+        "price": 190.99,
+        "image": "images-2.jpeg",
+        "_id": "2" #don't repeat this
+    },
+    {
+        "title": "Orange",
+        "category": "Fruit",
+        "price": 1.99,
+        "image": "somewhere",
+        "_id": "3" #don't repeat this
+    },
+    {
+        "title": "Peach",
+        "category": "Fruit",
+        "price": 3.99,
+        "image": "somewhere",
+        "_id": "4" #don't repeat this
+    },
+    {
+        "title": "Strawberry",
+        "category": "Fruit",
+        "price": 4.99,
+        "image": "somewhere",
+        "_id": "5" #don't repeat this
+    },
+    {
+        "title": "Kiwi",
+        "category": "Fruit",
+        "price": 5.99,
+        "image": "somewhere",
+        "_id": "6" #don't repeat this
+    },
+    {
+        "title": "Mango",
+        "category": "Fruit",
+        "price": 6.99,
+        "image": "somewhere",
+        "_id": "7" #don't repeat this
+    },
+    {
+        "title": "Grape",
+        "category": "Fruit",
+        "price": 7.99,
+        "image": "somewhere",
+        "_id": "8" #don't repeat this
+    },
+    {
+        "title": "Pineapple",
+        "category": "Fruit",
+        "price": 8.99,
+        "image": "somewhere",
+        "_id": "9" #don't repeat this
+    },
+    {
+        "title": "Watermelon",
+        "category": "Fruit",
+        "price": 9.99,
+        "image": "somewhere",
+        "_id": "10" #don't repeat this
+    },
+    {
+        "title": "Blueberry",
+        "category": "Fruit",
+        "price": 10.99,
+        "image": "somewhere",
+        "_id": "11" #don't repeat this
+    },
+    {
+        "title": "Raspberry",
+        "category": "Fruit",
+        "price": 11.99,
+        "image": "somewhere",
+        "_id": "12" #don't repeat this
+    },
+    {
+        "title": "Cherry",
+        "category": "Fruit",
+        "price": 12.99,
+        "image": "somewhere",
+        "_id": "13" #don't repeat this
+    },
+    {
+        "title": "Pear",
+        "category": "Fruit",
+        "price": 13.99,
+        "image": "somewhere",
+        "_id": "14" #don't repeat this
+    }
+]
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from config import me
+from mock_data import catalog
 import json
 
 
@@ -19,6 +20,8 @@ def test():
 # ######################################################################   API   ######################################################################
 
 
+
+
 @app.get("/api/version")
 def version():
     v = {
@@ -32,6 +35,43 @@ def version():
 @app.get("/api/about")
 def about():
     return json.dumps(me)
+
+
+@app.get("/api/catalog")
+def get_catalog():
+    return json.dumps(catalog)
+
+
+@app.post('/api/catalog')
+def save_product():
+    product = request.get_json()
+
+    product['_id'] = len(catalog)
+    catalog.append(product)
+
+    return json.dumps(product)
+
+
+# create a get /api/report/total
+# that send the total value of your catalog (sum of all prices)
+@app.get("/api/report/total")
+def get_report_total():
+    total = 0
+    for prod in catalog:
+        total += prod['price']
+    return json.dumps({"total": total})   
+
+# get all products for a given category
+# get /api/catalog/cat/<category>
+# get /api/catalog/cat/Fruit
+@app.get("/api/products/<cat>")
+def get_by_category(cat):
+    results = []
+    for prod in catalog:
+        if prod["category"] == cat:
+            results.append(prod)
+    return json.dumps(results)
+
 
 app.run(debug=True)
 
